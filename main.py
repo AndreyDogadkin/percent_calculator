d6e822ee22992adf391f6c1417ed75448a67c584
Andey Dogadkin <jvgger@gmail.com>
2023-03-08 13:24:20 +0300

    output

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from tkinter import *
-window = Tk()
-window.title('Percent calculator')
-window.geometry('300x300')
 
-label = Label(text='Введите сумму')
-e = Entry()
-b = Button(text='OK')
-
-label.pack()
-e.pack()
-b.pack()
+# ____________________
+# MAIN PROGRAM
+# ____________________
 
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-user_money = ''
+# user_money = ''
 
 
 def get_sum():
@@ -28,15 +21,16 @@ def get_key(per_cent, value):
 
 def percent_calc(money=1):
     try:
-        money = get_sum()
+        # money = get_sum()
         if money > 0:
             max_val = max(per_cent.values())
             bank = get_key(per_cent, max_val)
             profit = round(float(max_val / 100 * money), 2)
             profit_year = round(float(profit * 12), 2)
-            print(f'\nЛучшее предложение от {bank}, ставка {max_val}%. \n'
+            return_inf = (f'\nЛучшее предложение от {bank}, ставка {max_val}%. \n'
                   f'Ваша прибыль в месяц составит: {profit} рублей.\n'
                   f'Ваша прибыль в год составит: {profit_year} рублей.\n')
+            return return_inf
             all_banks(money)
             restart_calc()
         else:
@@ -47,17 +41,27 @@ def percent_calc(money=1):
         percent_calc(user_money)
 
 
+# def all_banks(money):
+#     all_b = input('Нажмите [ENTER] для расчета по всем возможным банкам или [ЛЮБОЙ СИМВОЛ] для выхода.\n')
+#     if all_b == '':
+#         for b, p in per_cent.items():
+#             profit = round(float(p / 100 * money), 2)
+#             profit_year = round(float(profit * 12), 2)
+#             bank_list = f'{b} ставка {str(p)} ваша прибыль составит: ' \
+#                         f'{profit} рублей в месяц и {profit_year} рублей в год.'
+#             return (f'{bank_list} \n')
+#     else:
+#         return ('')
+
 def all_banks(money):
-    all_b = input('Нажмите [ENTER] для расчета по всем возможным банкам или [ЛЮБОЙ СИМВОЛ] для выхода.\n')
-    if all_b == '':
-        for b, p in per_cent.items():
-            profit = round(float(p / 100 * money), 2)
-            profit_year = round(float(profit * 12), 2)
-            bank_list = f'{b} ставка {str(p)} ваша прибыль составит: ' \
-                        f'{profit} рублей в месяц и {profit_year} рублей в год.'
-            print(f'{bank_list} \n')
-    else:
-        print('')
+    out_list = ''
+    for b, p in per_cent.items():
+        profit = round(float(p / 100 * money), 2)
+        profit_year = round(float(profit * 12), 2)
+        bank_list = f'{b} ставка {str(p)} ваша прибыль составит: ' \
+                    f'{profit} рублей в месяц и {profit_year} рублей в год.'
+        out_list = out_list + bank_list + '\n'
+    return out_list
 
 
 def restart_calc():
@@ -68,7 +72,36 @@ def restart_calc():
         percent_calc(user_money)
     else:
         print('Досвидания!')
+# ____________
+# GUI
+# ____________
+def get_user_money():
+    get_value = user_money.get()
+    out_one_bank = percent_calc(get_value)
+    all_banks_get = all_banks(get_value)
+    banks_info['text'] = out_one_bank, all_banks_get
+
+
+window = Tk()
+window.title('Percent calculator')
+window.geometry('500x500')
+
+user_money = IntVar(value=1)
+all_banks_get = StringVar()
+banks_info = IntVar()
+label = Label(text='Введите сумму')
+e = Entry(textvariable=user_money)
+button_ok = Button(command=get_user_money, text='OK')
+banks_info = Label(text=percent_calc)
+button_all_banks = Button(command=all_banks, text='Все банки')
+
+label.pack()
+e.pack()
+button_ok.pack()
+banks_info.pack()
+# button_all_banks.pack()
+
 
 window.mainloop()
 
-percent_calc(user_money)
+# percent_calc(user_money)
